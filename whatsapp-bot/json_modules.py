12413285c15ee755fbbd1031df283d9c968973e3
kyang72 <kyang72@asu.edu>
2023-03-17 00:42:35 -0700

    Explore Python JSON modules

---
+++ whatsapp-bot/json_modules.py
@@ -0,0 +1,21 @@
+#Option 1
+import json
+json_person = '{"name": "Kevin", "age": 100, "hobbies": ["video games", "ultimate frisbee"], "appearance": {"hair_color": "black","weight": "350"}}'
+# JSON object -> Python dictionary
+person_dictionary = json.loads(json_person)
+person_dictionary.get("age")
+
+# Python object -> JSON string
+json_person = json.dumps(person_dictionary)
+# Pretty Print
+json.dumps(person_dictionary, sort_keys=True, indent=4)
+# sort_keys is optional
+# indent should be 2 or 4
+
+# Write to JSON File
+with open ("example.json", "w") as file:
+	file.write(json_person)
+
+# Read from JSON file
+with open("example.json", "r") as file:
+	json_content = json.loads(file.read())
