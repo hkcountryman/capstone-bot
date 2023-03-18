acf891e12ca7b81972ad6af9d240189d09dcea98
kyang72 <kyang72@asu.edu>
2023-03-18 12:35:17 -0700

    Experiment with JSONpickle module

--- whatsapp-bot/json_modules.py
+++ whatsapp-bot/json_modules.py
@@ -3,7 +3,7 @@ import json
 json_person = '{"name": "Kevin", "age": 100, "hobbies": ["video games", "ultimate frisbee"], "appearance": {"hair_color": "black","weight": "350"}}'
 # JSON object -> Python dictionary
 person_dictionary = json.loads(json_person)
-person_dictionary.get("age")
+person_dictionary.get("age") #100
 
 # Python object -> JSON string
 json_person = json.dumps(person_dictionary)
@@ -19,3 +19,23 @@ with open ("example.json", "w") as file:
 # Read from JSON file
 with open("example.json", "r") as file:
 	json_content = json.loads(file.read())
+
+
+#Option 2
+import jsonpickle
+python_obj = Thing('Example String')
+
+# Python object -> JSON string
+pickled = jsonpickle.encode(python_obj)
+# pickled = 
+#{
+	#"py/object": {
+		#"samples.Thing", 
+		#"name": "Example String", 
+		#"child": null
+	#}
+#}
+
+# JSON string -> Python obejct
+unpickled = jsonpickle.decode(pickled)
+str(unpickled.name) #Example String
