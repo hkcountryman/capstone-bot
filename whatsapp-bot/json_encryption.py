29f290d4c7151cf219e209a52f7c16388bd66b5b
kyang72 <kyang72@asu.edu>
2023-03-20 05:31:01 -0700

    Explore objcrypt encryption method

--- whatsapp-bot/json_encryption.py
+++ whatsapp-bot/json_encryption.py
@@ -22,3 +22,24 @@ encrypted_data = fernet.encrypt(data)
 # Decryption
 fernet = Fernet(key)
 decrypted_data = fernet.decrypt(data)
+
+
+#Option 2
+import json
+import objcrypt
+
+# 'Any key can be used in place of 'abc'
+crypter = objcrypt.Crypter('key', 'abc')
+python_dictionary = {
+	'name': 'kevin'
+}
+# Encrypt Python object
+encrypted_data = crypter.encrypt_object(python_dictionary)
+
+# Encrypt JSON
+json_dictionary = json.loads(python_dictionary)
+encrypted_json = crypter.encrypt_json(json_dictionary)
+
+# Decryption
+decrypted_data = crypter.decrypt_object(encrypted_data)
+decrypted_json = crypter.decrypt_json(encrypted_json)
